a3c91ab3297fcc55c63b0601b310a9c7e135d0cb
Samuel_Bakes <samuelbakes.pro21@gmail.com>
2020-11-01 05:22:18 -0700

    rdsth

--- Routes/rough.js
+++ Routes/rough.js
@@ -3,7 +3,7 @@ const express = require('express');
 const roughController = require('../Controllers/rough');
 const router = express.Router();
 router.use('movies', roughController.getMovies);
-router.use('index', roughController.getIndex);
+router.use('/index', roughController.getIndex);
 router.use('page', roughController.getPage);
 router.use('trickortreat', roughController.getTrickOrTreat);
 module.exports = router;
